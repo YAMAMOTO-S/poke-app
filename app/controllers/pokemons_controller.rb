e9be377c5f62cd36b67d7c39eae07b05251a251f
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-08 16:05:35 +0900

    first

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,42 @@
+class PokemonsController < ApplicationController
+
+  def index
+    @pokemons = Pokemon.all
+  end
+  
+
+  def new
+    return if params[:search].blank?
+    raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
+
+    if raw_response.status == 200
+      # ポケモンがあったら
+      response = JSON.parse(raw_response.body)
+      @pokemon = Pokemon.new(order: response["id"], name: response["name"], image_url: response["sprites"]["front_female"])
+    else
+      # なければ
+      redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー！"
+    end
+  end
+
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    
+    if @pokemon.save
+      redirect_to pokemons_path, notice: "You caught a #{@pokemon.name} !!"
+    else
+      render :new
+    end
+
+  end
+  
+
+  private
+  def pokemon_params
+    params.require(:pokemon).permit(:order, :name, :image_url)
+  end
+  
+end
+
+
