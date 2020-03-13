5d30d8a9c01d6a3f985c2951700880748c36ae18
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-13 17:45:59 +0900

    install BetterErrors

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -19,7 +19,7 @@ class PokemonsController < ApplicationController
       @pokemon = Pokemon.new(order: response['id'], 
         name: response['name'], image_url: response["sprites"]["front_default"], 
           type1: response['types'][0]['type']['name'],
-            description: textres['flavor_text_entries'][1]['flavor_text'])
+            description: textres['flavor_text_entries'][2]['flavor_text'])
 
     else
       # なければ
