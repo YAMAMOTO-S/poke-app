0fcd213ac5c2b7b2a0357b7ec4207b55c7f45bbb
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-13 15:32:26 +0900

    add text_description

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -9,11 +9,16 @@ class PokemonsController < ApplicationController
   def new
     return if params[:search].blank?
     raw_response = Faraday.get "https://pokeapi.co/api/v2/pokemon/#{params[:search]}"
+    text_response = Faraday.get "https://pokeapi.co/api/v2/pokemon-species/#{params[:search]}/"
 
     if raw_response.status == 200
       # ポケモンがあったら
       response = JSON.parse(raw_response.body)
-      @pokemon = Pokemon.new(order: response["id"], name: response["name"], image_url: response["sprites"]["front_default"])
+      textres = JSON.parse(text_response.body)
+
+      @pokemon = Pokemon.new(order: response['id'], 
+        name: textres['flavor_text_entries'][0]['flavor_text'], image_url: response["sprites"]["front_default"])
+
     else
       # なければ
       redirect_to new_pokemon_path, notice: "#{raw_response.status}エラー！"
