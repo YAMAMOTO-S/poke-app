58efcc76d57111644234f483f4f392a3fd5d5fb4
Shunya Yamamoto <yamasyunizig1216@gmail.com>
2020-03-20 19:41:23 +0900

    ajax to pokemons's likes at index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,13 +4,11 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(pokemon_id: params[:pokemon_id])
     favorite.save
-    redirect_back(fallback_location: '/pokemons/#{pokemon.id}')
   end
 
   def destroy
     favorite = Favorite.find_by(pokemon_id: params[:pokemon_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: '/pokemons/#{pokemon.id}')
   end
 
 
